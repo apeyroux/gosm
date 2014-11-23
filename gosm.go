037b6cb324a65d3de9475eb7eda600d9a3f5d76e
j4pe <m@j4.pe>
2014-11-23 10:42:13 +0100

    modif

--- gosm.go
+++ gosm.go
@@ -24,13 +24,13 @@ type Conversion interface {
 	Num2deg(t *Tile) (lat float64, long float64)
 }
 
-func (*Tile) Deg2num(t *Tile) (x int, y int) {
+func (t *Tile) Deg2num() (x int, y int) {
 	x = int(math.Floor((t.Long + 180.0) / 360.0 * (math.Exp2(float64(t.Z)))))
 	y = int(math.Floor((1.0 - math.Log(math.Tan(t.Lat*math.Pi/180.0)+1.0/math.Cos(t.Lat*math.Pi/180.0))/math.Pi) / 2.0 * (math.Exp2(float64(t.Z)))))
 	return
 }
 
-func (*Tile) Num2deg(t *Tile) (lat float64, long float64) {
+func (t *Tile) Num2deg() (lat float64, long float64) {
 	n := math.Pi - 2.0*math.Pi*float64(t.Y)/math.Exp2(float64(t.Z))
 	lat = 180.0 / math.Pi * math.Atan(0.5*(math.Exp(n)-math.Exp(-n)))
 	long = float64(t.X)/math.Exp2(float64(t.Z))*360.0 - 180.0
